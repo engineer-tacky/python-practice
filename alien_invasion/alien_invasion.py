3862100ff411a828fe5fa8b002f923e356b5a973
engineer_tacky <tsubasa801@gmail.com>
2022-02-23 14:07:50 +0900

    ゲームのウィンドウ表示・背景色・画像表示まで作成

---
+++ alien_invasion/alien_invasion.py
@@ -0,0 +1,43 @@
+import sys
+
+import pygame
+
+from settings import Settings
+from ship import Ship
+
+
+class AlienInvasion:
+    def __init__(self):
+        pygame.init()
+        self.settings = Settings()
+
+        self.screen = pygame.display.set_mode(
+            (
+                self.settings.screen_width,
+                self.settings.screen_height
+            )
+        )
+        pygame.display.set_caption("エイリアン侵略")
+
+        self.ship = Ship(self)
+
+        self.bg_color = (230, 230, 230)
+
+    def run_game(self):
+        while True:
+            self._check_events()
+            self._update_screen()
+
+    def _check_events(self):
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                sys.exit()
+
+    def _update_screen(self):
+        self.screen.fill(self.settings.bg_color)
+        self.ship.blitme()
+        pygame.display.flip()
+
+if __name__ == '__main__':
+    ai = AlienInvasion()
+    ai.run_game()
