8709dabf0c1829f2f96663a3ae111f46e78eab6b
engineer_tacky <tsubasa801@gmail.com>
2022-02-23 14:36:32 +0900

    宇宙船の左右の移動を実装 & リファクタリング

--- alien_invasion/alien_invasion.py
+++ alien_invasion/alien_invasion.py
@@ -17,7 +17,14 @@ class AlienInvasion:
                 self.settings.screen_height
             )
         )
-        pygame.display.set_caption("エイリアン侵略")
+
+        # self.screen = pygame.display.set_mode(
+        #     (0, 0),
+        #     pygame.FULLSCREEN
+        # )
+        # self.settings.screen_width = self.screen.get_rect().width
+        # self.settings.screen_height = self.screen.get_rect().height
+        # pygame.display.set_caption("エイリアン侵略")
 
         self.ship = Ship(self)
 
@@ -26,12 +33,31 @@ class AlienInvasion:
     def run_game(self):
         while True:
             self._check_events()
+            self.ship.update()
             self._update_screen()
 
     def _check_events(self):
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 sys.exit()
+            elif event.type == pygame.KEYDOWN:
+                self._check_keydown_events(event)
+            elif event.type == pygame.KEYUP:
+                self._check_keyup_events(event)
+
+    def _check_keydown_events(self, event):
+        if event.key == pygame.K_RIGHT:
+            self.ship.moveing_right = True
+        elif event.key == pygame.K_LEFT:
+            self.ship.moveing_left = True
+        elif event.key == pygame.K_q:
+            sys.exit()
+
+    def _check_keyup_events(self, event):
+        if event.key == pygame.K_RIGHT:
+            self.ship.moveing_right = False
+        if event.key == pygame.K_LEFT:
+            self.ship.moveing_left = False
 
     def _update_screen(self):
         self.screen.fill(self.settings.bg_color)
