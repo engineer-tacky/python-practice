05075704f08309eda37ea7b1a9e2daac0169d9b4
engineer_tacky <tsubasa801@gmail.com>
2022-02-27 16:56:34 +0900

    CSVの取り扱い

---
+++ graph/sitka_highs.py
@@ -0,0 +1,34 @@
+from cProfile import label
+import csv
+from datetime import datetime
+
+from shutil import which
+
+import matplotlib.pyplot as plt
+
+filename = 'data/sitka_weather_2018_simple.csv'
+with open(filename) as f:
+    reader = csv.reader(f)
+    header_row = next(reader)
+
+    dates, highs = [], []
+
+    for row in reader:
+        current_date = datetime.strptime(row[2], '%Y-%m-%d')
+        high = int(row[5])
+        dates.append(current_date)
+        highs.append(high)
+
+    print(highs)
+
+plt.style.use('seaborn')
+fig, ax = plt.subplots()
+ax.plot(dates, highs, c='red')
+
+plt.title("Daily high temperatures - 2018", fontsize=24)
+plt.xlabel('', fontsize=16)
+fig.autofmt_xdate()
+plt.ylabel("Temperature (F)", fontsize=16)
+plt.tick_params(axis='both', which='major', labelsize=16)
+
+plt.show()
